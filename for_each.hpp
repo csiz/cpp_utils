f5c721bc327875a566389d14c728a4e2bf775313
Calin <calin.mocanu@gmail.com>
2016-03-12 21:29:24 +0000

    Don't start identifiers with underscore.

--- for_each.hpp
+++ for_each.hpp
@@ -2,8 +2,8 @@
 #include <type_traits>
 #include <utility>
 
-namespace _detail_for_each {
-  namespace _detail_unpack_call {
+namespace detail_for_each {
+  namespace detail_unpack_call {
 
     template<typename Func, typename Tuple, size_t ... S>
     auto unpack_call_impl(Func && func, Tuple && tuple, std::index_sequence<S ...>) 
@@ -12,20 +12,20 @@ namespace _detail_for_each {
       return std::forward<Func>(func)(std::get<S>(std::forward<Tuple>(tuple)) ...);
     }
 
-    template<typename Func, typename Tuple, size_t _size = std::tuple_size<typename std::remove_reference<Tuple>::type>::value>
+    template<typename Func, typename Tuple, size_t size = std::tuple_size<typename std::remove_reference<Tuple>::type>::value>
     auto unpack_call_sfinae(Func && func, Tuple && tuple)
-      -> decltype(unpack_call_impl(std::forward<Func>(func), std::forward<Tuple>(tuple), std::make_index_sequence<_size>()))
+      -> decltype(unpack_call_impl(std::forward<Func>(func), std::forward<Tuple>(tuple), std::make_index_sequence<size>()))
     {
-      return unpack_call_impl(std::forward<Func>(func), std::forward<Tuple>(tuple), std::make_index_sequence<_size>());
+      return unpack_call_impl(std::forward<Func>(func), std::forward<Tuple>(tuple), std::make_index_sequence<size>());
     }
   }
 
   /** Unpack tuple elements and use them as parameters for function. */
   template<typename Func, typename Tuple>
   auto unpack_call(Func && func, Tuple && tuple) 
-    -> decltype(_detail_unpack_call::unpack_call_sfinae(std::forward<Func>(func), std::forward<Tuple>(tuple)))
+    -> decltype(detail_unpack_call::unpack_call_sfinae(std::forward<Func>(func), std::forward<Tuple>(tuple)))
   {
-    return _detail_unpack_call::unpack_call_sfinae(std::forward<Func>(func), std::forward<Tuple>(tuple));
+    return detail_unpack_call::unpack_call_sfinae(std::forward<Func>(func), std::forward<Tuple>(tuple));
   }
 
   namespace _detail_call {  
@@ -61,6 +61,6 @@ template<typename Range, typename Func>
 void for_each(Range && range, Func && func){
   for (auto && element : range){
     using Element = decltype(element);
-    _detail_for_each::call(std::forward<Func>(func), std::forward<Element>(element));
+    detail_for_each::call(std::forward<Func>(func), std::forward<Element>(element));
   }
 }
