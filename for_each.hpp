6dbe55bc22f298ff629779d977f1a05a30e09be9
Calin <csiz@users.noreply.github.com>
2016-03-14 09:18:19 +0000

    Add boost license.

--- for_each.hpp
+++ for_each.hpp
@@ -1,3 +1,8 @@
+//          Copyright csiz.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include <tuple>
 #include <type_traits>
 #include <utility>
