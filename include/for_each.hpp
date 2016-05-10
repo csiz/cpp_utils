6dd1e3da0a7a32928b34fc64b011813db8df1a00
Calin Mocanu <calin.mocanu@oxam.com>
2016-05-10 12:36:59 +0100

    Add documentation for parameters.

--- include/for_each.hpp
+++ include/for_each.hpp
@@ -6,6 +6,9 @@ namespace csiz {
   /** Apply function to each element in range. 
     
     If the range elements are tuples unpack them as function parameters.
+
+    @param range SinglePassRange to iterate over.
+    @param func Function to call for each element.
   */
   template<typename Range, typename Func>
   void for_each(Range && range, Func && func){
